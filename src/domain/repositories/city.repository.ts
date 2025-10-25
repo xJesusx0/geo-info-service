2936eab58373d97529663c45f6f0d6e26040c298
xJesusx0 <snowcatyt1234@gmail.com>
2025-10-24 20:06:11 -0500

    feat: implementar cliente de supabase

--- src/domain/repositories/city.repository.ts
+++ src/domain/repositories/city.repository.ts
@@ -2,5 +2,5 @@ import { City } from "../../types/city";
 
 export interface CityRepository {
   findAll(): Promise<City[]>;
-  findById(id: number): Promise<City>;
+  findById(id: number): Promise<City | null>;
 }
--- src/infraestructure/repositories/city.repository.ts
+++ src/infraestructure/repositories/city.repository.ts
@@ -1,12 +1,33 @@
+import { SupabaseClient } from "@supabase/supabase-js";
 import { CityRepository } from "../../domain/repositories/city.repository";
 import { City } from "../../types/city";
+import { Database } from "../../types/database.types";
 
 export class SupabaseCityRepository implements CityRepository {
-  findAll(): Promise<City[]> {
-    return Promise.resolve([]);
+  constructor(private supabaseClient: SupabaseClient<Database>) {}
+
+  async findAll(): Promise<City[]> {
+    const { data, error } = await this.supabaseClient.from("city").select("*");
+    if (error) {
+      console.error(error);
+      throw new Error(error.message);
+    }
+
+    return data;
   }
 
-  findById(id: number): Promise<City> {
-    return Promise.reject(new Error("Method not implemented."));
+  async findById(id: number): Promise<City | null> {
+    const { data, error } = await this.supabaseClient
+      .from("city")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.error(error);
+      throw new Error(error.message);
+    }
+
+    return data;
   }
 }
